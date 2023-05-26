32ed90c28facbf80854c5575392055118d8df9ad
Harsh Patel <harsh3608@gmail.com>
2023-05-26 17:51:30 +0530

    created seller menu and side-nav

--- src/app/seller/seller.module.ts
+++ src/app/seller/seller.module.ts
@@ -1,17 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SellerRoutingModule } from './seller-routing.module';
 import { SellerHomePageComponent } from './seller-home-page/seller-home-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ToastrModule } from 'ngx-toastr';
+import { UserRoutingModule } from '../user/user-routing.module';
+import { SellerMenuComponent } from './seller-menu/seller-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
   declarations: [
-    SellerHomePageComponent
+    SellerHomePageComponent,
+    SellerMenuComponent
   ],
   imports: [
     CommonModule,
-    SellerRoutingModule
+    SellerRoutingModule,
+    UserRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ]
 })
 export class SellerModule { }
