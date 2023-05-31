3bb2f7111386933a93c75b86dd2a4e0361e11265
Harsh Patel <harsh3608@gmail.com>
2023-05-31 17:59:54 +0530

    almost done change password component

--- src/app/seller/seller-menu/seller-menu.component.ts
+++ src/app/seller/seller-menu/seller-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { PasswordChangeComponent } from 'src/app/user/password-change/password-change.component';
 import { AuthService } from 'src/app/user/shared/authorization/auth.service';
 
 @Component({
@@ -12,13 +14,24 @@ export class SellerMenuComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
+    public dialog: MatDialog,
   ){}
 
   ngOnInit(): void {
     
   }
+
+  openChangePasswordDialog(){
+    const dialogRef = this.dialog.open(PasswordChangeComponent,
+      {
+        data: {  }
+      }
+    );
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
   
-  LogOut(){
+  logOut(){
     this.authService.removeToken();
     this.router.navigate(['/']);
   }
--- src/app/user/password-change/password-change.component.ts
+++ src/app/user/password-change/password-change.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { ChangePassword } from '../shared/models/passwords.models';
+import { AuthService } from '../shared/authorization/auth.service';
 
 @Component({
   selector: 'app-password-change',
   templateUrl: './password-change.component.html',
   styleUrls: ['./password-change.component.css']
 })
-export class PasswordChangeComponent {
+export class PasswordChangeComponent implements OnInit {
+  changePassForm!: FormGroup;
+  public showPassword: boolean = false;
+  public showCurrentPassword: boolean = false;
+  ChangePasswordRequest!: ChangePassword;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+    this.changePassForm = new FormGroup({
+      email: new FormControl(this.authService.getUserMail()),
+      currentPassword: new FormControl(''),
+      newPassword: new FormControl(''),
+      confirmNewPassword: new FormControl('')
+    })
+  }
+
+  //Validator Function to check password match in password and confirm-password fields
+  checkPasswords: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    let pass = control.get('password')?.value
+    let confirmPass = control.get('confirmPassword')?.value
+    return pass == confirmPass ? null : { notSame: true }
+  }
+
+  //Function to toggle password visibility in password fields
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  public toggleCurrentPasswordVisibility(): void {
+    this.showCurrentPassword = !this.showCurrentPassword;
+  }
+
+
+  submitForm() {
+    this.changePassForm.markAllAsTouched();
+    if (this.changePassForm.valid) {
+      this.ChangePasswordRequest = this.changePassForm.value;
+      console.log(this.ChangePasswordRequest);
+    }
+  }
+
+  get currentPassword(): FormControl {
+    return this.changePassForm.get("currentPassword") as FormControl;
+  }
+
+  get newPassword(): FormControl {
+    return this.changePassForm.get("newPassword") as FormControl;
+  }
+
+  get confirmNewPassword(): FormControl {
+    return this.changePassForm.get("confirmNewPassword") as FormControl;
+  }
 
 }
