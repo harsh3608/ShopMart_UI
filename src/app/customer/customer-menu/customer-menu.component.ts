4e33e763ece1cbea2f404d22150b1580533f0bc3
Harsh Patel <harsh3608@gmail.com>
2023-06-05 11:28:54 +0530

    completed customer menu

--- src/app/customer/customer-menu/customer-menu.component.ts
+++ src/app/customer/customer-menu/customer-menu.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { PasswordChangeComponent } from 'src/app/user/password-change/password-change.component';
+import { AuthService } from 'src/app/user/shared/authorization/auth.service';
 
 @Component({
   selector: 'app-customer-menu',
   templateUrl: './customer-menu.component.html',
   styleUrls: ['./customer-menu.component.css']
 })
-export class CustomerMenuComponent {
+export class CustomerMenuComponent implements OnInit{
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public dialog: MatDialog,
+  ) 
+  { }
+
+  ngOnInit(): void {
+    
+  }
+
+  openChangePasswordDialog(){
+    const dialogRef = this.dialog.open(PasswordChangeComponent,
+      {
+        data: {  }
+      }
+    );
+    dialogRef.addPanelClass('rounded-dialog-container');
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
+  
+  logOut(){
+    this.authService.removeToken();
+    this.router.navigate(['/']);
+  }
+
+  getPersonName(){
+    return this.authService.getPersonName();
+  }
 }
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,6 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerMenuComponent } from './customer-menu/customer-menu.component';
 import { CustomerHomePageComponent } from './customer-home-page/customer-home-page.component';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToastrModule } from 'ngx-toastr';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,7 +21,15 @@ import { CustomerHomePageComponent } from './customer-home-page/customer-home-pa
   ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    ToastrModule.forRoot(),
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
   ]
 })
 export class CustomerModule { }
