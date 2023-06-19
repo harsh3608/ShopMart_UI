8c7e3da6a283234ec37f2982520a1de84bdbd05c
Harsh Patel <harsh3608@gmail.com>
2023-06-19 19:30:24 +0530

    changes in cart component

--- src/app/customer/products-pool/products-pool.component.ts
+++ src/app/customer/products-pool/products-pool.component.ts
@@ -65,7 +65,6 @@ export class ProductsPoolComponent implements OnInit{
         if(res.isSuccess){
           this.cartCount = res.response.length;
           this.cartCountChange.emit(this.cartCount);
-          console.log(this.cartCount);
         }
       }
     )
