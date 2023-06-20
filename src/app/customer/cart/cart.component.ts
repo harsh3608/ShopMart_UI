e8a372b5eadd818b97c13961fc8302409d3b39e0
Harsh Patel <harsh3608@gmail.com>
2023-06-20 18:29:22 +0530

    cart added functionality

--- src/app/customer/cart/cart.component.ts
+++ src/app/customer/cart/cart.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/user/shared/authorization/auth.service';
 import { CartService } from '../shared/services/cart.service';
-import { CartProduct } from '../shared/models/cart-models';
+import { CartProduct, CartProductRequest } from '../shared/models/cart-models';
 import { ProductService } from 'src/app/seller/shared/services/product.service';
-import { Product } from 'src/app/seller/shared/models/Product-models';
+import { Category, Product } from 'src/app/seller/shared/models/Product-models';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CategoryService } from 'src/app/seller/shared/services/category.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -20,18 +21,29 @@ export class CartComponent implements OnInit{
   products: Product[]=[];
   imageBaseLink: string = "https://localhost:7071/resources/";
   uniqueProducts: Product[]=[];
+  totalPrice:number = 0;
+  categories: Category[]=[];
+  category!: Category;
+  AddCartRequest!: CartProductRequest;
 
   constructor(
     private authService: AuthService,
     private cartService: CartService,
     private toastr: ToastrService,
-    private productService: ProductService
+    private productService: ProductService,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
     this.cartId = this.authService.getCartId();
-    this.GetAllCartProductsAndCount();
-    this.GetUniqueProducts();
+    this.GetAllCartProductsAndCount(); 
+    this.categoryService.getCategories().subscribe(
+      (res) => {
+        if(res.isSuccess){
+          this.categories = res.response;
+        };
+      }
+    );
   }
 
 
@@ -41,12 +53,22 @@ export class CartComponent implements OnInit{
         if(res.isSuccess){
           this.cartCount = res.response.length;
           this.cartProducts = res.response;
+          // Create a set to track unique product IDs
+          const uniqueProductIds = new Set();
           //to create array of products drom cart products
           for (const cartProduct of this.cartProducts) {
            this.GetProductById(cartProduct.productId).subscribe(res=>{
-              this.products.push(res);               
-            });            
-          }    
+              this.products.push(res); 
+              this.totalPrice += res.price;             
+            });  
+            //
+            if (!uniqueProductIds.has(cartProduct.productId)) {
+              uniqueProductIds.add(cartProduct.productId); // Add the product ID to the set
+              this.GetProductById(cartProduct.productId).subscribe(res => {
+                this.uniqueProducts.push(res);
+              });
+            }          
+          };    
            
         };
       }
@@ -54,8 +76,17 @@ export class CartComponent implements OnInit{
   }
 
 
+  GetCategoryById(categoryId:any): string {
+    let name = 'name';
+    this.categories.forEach((item)=>{
+      if(item.categoryId == categoryId) {
+        name =  item.categoryName
+      }
+    })
+    return name;
+  }
 
-  
+
   //To get whole product from product id
   GetProductById(productId: any): Observable<Product> {
     return this.productService.getProductById(productId).pipe(
@@ -67,26 +98,66 @@ export class CartComponent implements OnInit{
       })
     );
   }
-
-  GetUniqueProducts() {
-
-    console.log(this.products);
-    for(const product of this.products){
-      console.log(product);
-    }
-    
-    
-
-
-    // this.uniqueProducts = this.products.filter((product, index) => uniqueIds.indexOf(product.id) === index);
-    
-    //console.log(this.uniqueProducts);
-  }
-
+ 
   //get qty of product in the cart
   GetQuantity(item: any): number {
     const count = this.products.filter((product) => product.name === item.name).length;
     return count > 1 ? count : 1;
   }
+
+  
+
+  // GetAllCartProductsCount() {
+  //   this.cartService.GetAllCartProducts(this.cartId).subscribe(
+  //     (res) => {
+  //       if(res.isSuccess){
+  //         this.cartCount = res.response.length;
+  //       }
+  //     }
+  //   )
+  // }
+
+  AddProductToCart(productId: any){
+    this.AddCartRequest = {
+      cartId: this.cartId,
+      productId: productId
+    }
+    this.cartService.AddCartProduct(this.AddCartRequest).subscribe(
+      (res)=> {
+        if(res.isSuccess){
+          this.toastr.success('Product Added to Cart Successfully!', 'Success!',{
+            timeOut: 2000,
+          });
+        }
+      }
+    );
+    this.cartCount = 0;
+    this.cartProducts.length = 0;
+    this.products.length = 0;
+    this.uniqueProducts.length = 0;
+    this.GetAllCartProductsAndCount();
+  }
+
+
+  RemoveProductFromCart(productId: any){
+    this.cartService.RemoveCartProduct(this.cartId, productId).subscribe(
+      (res)=> {
+        if(res.isSuccess){
+          this.toastr.warning('Product removed from shopping Cart !', 'Warning!',{
+            timeOut: 2000,
+          });
+        }
+      }
+    );
+    this.GetAllCartProductsAndCount();
+  }
+
+
+
+
+
+
+
+
 
 }
--- src/app/seller/shared/services/category.service.ts
+++ src/app/seller/shared/services/category.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { CategoryResponse } from "../models/Product-models";
+import { CategoryResponse, CategoryReturnResponse } from "../models/Product-models";
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class CategoryService{
-    productBaseServerLink: string = 'https://localhost:7071/api/v1/Category/';
+  providedIn: 'root'
+})
+
+export class CategoryService {
+  productBaseServerLink: string = 'https://localhost:7071/api/v1/Category/';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(
@@ -18,4 +19,8 @@ import { CategoryResponse } from "../models/Product-models";
     return this.http.get<CategoryResponse>(this.productBaseServerLink+'GetAllCategories', { headers: this.headers });
   }
 
+  getCategoryById(categoryId:any): Observable<CategoryReturnResponse> {
+    return this.http.get<CategoryReturnResponse>(this.productBaseServerLink+'GetCategoryById/'+categoryId, { headers: this.headers })
   }
+
+}
